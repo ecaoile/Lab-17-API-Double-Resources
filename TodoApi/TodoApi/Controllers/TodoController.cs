4bd95bb08a4ca35d50743ddaea29626b5137a4f2
ecaoile <unfie555@gmail.com>
2018-06-28 11:11:59 -0700

    CRUD working with API

---
+++ TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        // I'm making this async even though the docs don't mention it. If something breaks, try changing it back.
+        [HttpGet("{id}", Name = "GetTodo")]
+        public async Task<ActionResult<TodoItem>> GetById(long id)
+        {
+            var item = await _context.TodoItems.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        // I'm making this async even though the docs don't mention it. If something breaks, try changing it back.
+        [HttpPost]
+        public async Task<IActionResult> Create (TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute("GetTodo", new { id = item.ID }, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, TodoItem item)
+        {
+            var todo = await _context.TodoItems.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.TodoItems.Update(todo);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var todo = _context.TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
