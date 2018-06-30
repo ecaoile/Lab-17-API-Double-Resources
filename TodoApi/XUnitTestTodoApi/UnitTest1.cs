a4ed56fb62d680a715d2aee0d40949b65c402e28
ecaoile <unfie555@gmail.com>
2018-06-29 23:31:15 -0700

    full crud testing on todo items

--- TodoApi/XUnitTestTodoApi/UnitTest1.cs
+++ TodoApi/XUnitTestTodoApi/UnitTest1.cs
@@ -22,31 +22,21 @@ namespace XUnitTestTodoApi
             using (TodoDbContext context = new TodoDbContext(options))
             {
                 // Arrange
-                TodoItem datItem1 = new TodoItem();
-                datItem1.Name = "walk the dog";
-                datItem1.IsComplete = false;
-                //datItem1.DatListID = 1;
-
-                TodoItem datItem2 = new TodoItem();
-                datItem2.Name = "wash the dishes";
-                datItem2.IsComplete = true;
-                //datItem1.DatListID = 1;
+                TodoItem datItem = new TodoItem();
+                datItem.ID = 5;
+                datItem.Name = "creating a test method";
+                datItem.IsComplete = false;
 
                 TodoController lc = new TodoController(context);
 
                 // Act
-                var result1 = lc.Create(datItem1);
-                var result2 = lc.Create(datItem2);
+                var result = lc.Create(datItem);
 
-                var answer1 = result1.Result;
-                var x = (ObjectResult)answer1;
-
-                var answer2 = result2.Result;
-                var y = (ObjectResult)answer2;
+                var answer = result.Result;
+                var x = (ObjectResult)answer;
 
                 // Assert
                 Assert.Equal(HttpStatusCode.Created, (HttpStatusCode)x.StatusCode);
-                Assert.Equal(HttpStatusCode.Created, (HttpStatusCode)y.StatusCode);
             }
         }
 
@@ -94,12 +84,12 @@ namespace XUnitTestTodoApi
             {
                 // Arrange
                 TodoItem datItem1 = new TodoItem();
-                datItem1.ID = 1;
+                datItem1.ID = 6;
                 datItem1.Name = "walk the dog";
                 datItem1.IsComplete = false;
 
                 TodoItem datItem2 = new TodoItem();
-                datItem1.ID = 2;
+                datItem2.ID = 7;
                 datItem2.Name = "wash the dishes";
                 datItem2.IsComplete = true;
 
@@ -116,6 +106,40 @@ namespace XUnitTestTodoApi
                 Assert.Equal(datItem2.Name, result2.Result.Value.Name);
                 Assert.Equal(datItem2.IsComplete, result2.Result.Value.IsComplete);
                 Assert.Equal(datItem2.DatListID, result2.Result.Value.DatListID);
+            }
+        }
+
+        // 3. Update a ToDo item
+        [Fact]
+        public void CanDeleteTodoItem()
+        {
+            DbContextOptions<TodoDbContext> options = new DbContextOptionsBuilder<TodoDbContext>().UseInMemoryDatabase("DatDatabase").Options;
+
+            using (TodoDbContext context = new TodoDbContext(options))
+            {
+                // Arrange
+                TodoItem datItem1 = new TodoItem();
+                datItem1.ID = 8;
+                datItem1.Name = "walk the dog";
+                datItem1.IsComplete = false;
+
+                TodoItem datItem2 = new TodoItem();
+                datItem2.ID = 9;
+                datItem2.Name = "wash the dishes";
+                datItem2.IsComplete = true;
+
+                TodoController lc = new TodoController(context);
+
+                // Act
+                var created1 = lc.Create(datItem1);
+                var created2 = lc.Create(datItem2);
+
+                var deletedItem = lc.Delete(datItem1.ID);
+
+                var result1 = lc.GetById(datItem1.ID);
+                var result2 = lc.GetById(datItem2.ID);
+
+                // Assert
             }
         }
     }
